bbe351f40c8e39d57cc2c7cc388d2e6326e044f2
Hasnah Nur Ardita <66120966+nadhaalditha@users.noreply.github.com>
2020-07-08 23:22:44 +0700

    Create asc_data.js

---
+++ asc_data.js
@@ -0,0 +1,13 @@
+const server = 'http://api.haxors.or.id';
+
+export default {
+  asyncData ({ params, error }) {
+    return axios.get(server)
+      .then((res) => {
+        return { title: res.data.title }
+      })
+      .catch((e) => {
+        error({ statusCode: 404, message: 'Post not found' })
+      })
+  }
+}
